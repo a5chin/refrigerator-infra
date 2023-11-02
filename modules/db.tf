96e104106cedc626712ad074a7a23c54879bb149
eigo-hara <eigo.hara@dena.com>
2023-11-03 01:14:24 +0900

    update: db for vpc

--- modules/db.tf
+++ modules/db.tf
@@ -1,28 +1,29 @@
 resource "google_sql_database_instance" "ref" {
   name             = "ref"
-  database_version = "POSTGRES_15"
+  database_version = "MYSQL_8_0"
 
   settings {
     tier = "db-f1-micro"
 
-    database_flags {
-      name  = "cloudsql.iam_authentication"
-      value = "on"
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.main.self_link
+      require_ssl     = false
     }
   }
 
-  deletion_protection = "false"
+  deletion_protection = false
 }
 
 resource "google_sql_database" "food" {
   name      = var.db.database_name
   instance  = google_sql_database_instance.ref.name
-  charset   = "UTF8"
-  collation = "en_US.UTF8"
+  charset   = var.db.charset
+  collation = var.db.collation
 }
 
-resource "google_sql_user" "iam_service_account_user" {
-  name     = trimsuffix(google_service_account.run_executor.email, ".gserviceaccount.com")
+resource "google_sql_user" "main" {
+  name     = "root"
   instance = google_sql_database_instance.ref.name
-  type     = "CLOUD_IAM_SERVICE_ACCOUNT"
+  password = ""
 }
